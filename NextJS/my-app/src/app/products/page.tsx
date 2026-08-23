4263d98d51c88c9ebf250410445d8686c0109e50
Foolmannn <mannmagar19@gmail.com>
2026-08-23 13:22:09 +0000

    Full Stack : Next js basics : Dynamic routes

---
+++ NextJS/my-app/src/app/products/page.tsx
@@ -0,0 +1,14 @@
+export default function ProductList(){
+    return (
+        <>
+
+        <h1>Product list </h1>
+        <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+            <li>Product 4</li>
+        </ul>
+        </>
+    )
+}
