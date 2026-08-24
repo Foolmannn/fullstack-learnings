929b62c762af134dd528a0ed1fc35888893dbfc3
Foolmannn <mannmagar19@gmail.com>
2026-08-24 13:22:50 +0000

    FullStack : next js - File colocation

---
+++ NextJS/my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export function BarChart(){
+    return <h1>Bar Chart</h1>
+}
+
+// So we can see that only the defaul exported react component is accessible as public in the browser in next js . 
+
+// So we can easily colocate the files in the app folder without worrying it may get revealed 
+
+export default function Dashboard(){
+    return <h1>Dashboard</h1>
+}
