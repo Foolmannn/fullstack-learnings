ea4b72ae4a0aec9c314e105b0794d0ea5e7e1605
Foolmannn <mannmagar19@gmail.com>
2026-08-27 15:12:53 +0000

    Fullstack : Next js - Link

--- NextJS/my-app/src/app/(auth)/layout.tsx
+++ NextJS/my-app/src/app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
 
 //  SO WITH THE HELP OF THE ROUTE GROUPS WE CAN DEFINE THE MULTIPLE ROOTLAYOUT WHICH HELPS US TO APPLY DIFFERENT LAYOUT TO DIFFERENT TYPE OF THE PAGES . 
+'use client'
+
 import type { Metadata } from "next";
+import '../globals.css'
+
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,18 +19,33 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Welcome to First Next App.",
-  description: "Learning Next js ",
-};
+// export const metadata: Metadata = {
+//   title: "Welcome to First Next App.",
+//   description: "Learning Next js ",
+// };
+
+const navLinks = [
+  {name:"Register",href:"/register"},
+  {name:"Login",href:"/login"},
+  {name:"Forgot-Password",href:"/forgot-password"},
+]
 
 export default function AuthLayout({ children }: LayoutProps<"/">) {
+  const pathname = usePathname()
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {navLinks.map((link)=>{
+          const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !=='/')
+          return (
+            <Link href={link.href} key={link.name} style={{padding:'10px'}} className={isActive ? "font-bold mr-4": 'text-blue-500 mr-4'}>
+              {link.name}
+            </Link>
+          )
+        })}
 
         {children}
 
