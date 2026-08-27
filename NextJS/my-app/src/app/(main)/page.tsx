9db4322082397368640aa08093a62f72e7d5a217
Foolmannn <mannmagar19@gmail.com>
2026-08-27 13:21:44 +0000

    Fullstack : Next js - Link

--- NextJS/my-app/src/app/(main)/page.tsx
+++ NextJS/my-app/src/app/(main)/page.tsx
@@ -1,9 +1,14 @@
-
 import { Metadata } from "next";
 
+import Link from "next/link";
 
-
-
-export default function Home(){
-    return <h1>Welcome Home ! </h1>
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome Home ! </h1>
+      <Link href="/blog">Blog</Link>
+      <br></br>
+      <Link href='/products'>Products</Link>
+    </>
+  );
 }
--- NextJS/my-app/src/app/(main)/products/page.tsx
+++ NextJS/my-app/src/app/(main)/products/page.tsx
@@ -1,15 +1,19 @@
 
-
+import Link from "next/link"
 export default function ProductList(){
+    const productId=100;
     return (
         <>
-
+        <Link href='/'>Home</Link>
         <h1>Product list </h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-            <li>Product 4</li>
+            <li><Link href='/products/1'>Product 1</Link></li>
+            <li><Link href='/products/2'>Product 2</Link></li>
+            <li><Link href='/products/3'>Product 3</Link></li>
+            <li><Link href='/products/4' replace>Product 4</Link></li> 
+            {/* this replace keyword will overwrite the history so when we back it will lead to home page instead of the product list page */}
+            <li><Link href={`/products/${productId}`}>Product {productId}</Link></li>
+       
         </ul>
         </>
     )
