b32a3205dab1488bd3c20c3e9a19da20316d8cea
Foolmannn <mannmagar19@gmail.com>
2026-08-29 12:57:50 +0000

    Fullstack : Next js - Error handling

--- NextJS/my-app/src/app/(main)/products/[productId]/reviews/[reviewId]/error.tsx
+++ NextJS/my-app/src/app/(main)/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,13 +1,29 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
 import React from 'react'
 
-function ErrorBoundary({error}:{error:Error}) { // this gets the error object which can be used 
+function ErrorBoundary({error,reset}:{error:Error,
+    reset: ()=> void;
+}) { // this gets the error object which can be used to receive the reset and message
+// better approach is to use the router to reload or transition
+    const router = useRouter();
+    const reload = () =>{
+        startTransition(()=>{
+            router.refresh();
+            reset();
+        })
+    }
   return (
     <div> 
         {/* Error in review Id  */}
-        {error.message}
-
+        <p>
+         {error.message}
+        </p>
+        {/* <button onClick={reset}>Try Again</button> */}
+        <button onClick={reload}>Try Again</button>
     </div>
   )
 }
